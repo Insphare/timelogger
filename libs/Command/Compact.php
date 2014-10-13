c098f03fb7f4d0bf4b9e0d8654116bfbf812465d
manuel <insphare@gmail.com>
2014-10-13 18:19:39 +0200

    add command compact

--- libs/Command/Compact.php
+++ libs/Command/Compact.php
@@ -9,44 +9,7 @@
 class Command_Compact extends Command_Show {
 
 	public function execute() {
-		$workObjects = $this->getWorkedObjects(time());
-
-		if (empty($workObjects)) {
-			return 'There are no logs, yet.';
-		}
-
-		$spaceLabel = Command_Abstract::WORK_LENGTH_NAME;
-		$spaceWork = 7;
-		$spaceNotes = 30;
-
-		$output = array();
-		$output[] = '';
-		//$output[] = '┌' . str_pad('', $spaceLabel, '─', STR_PAD_RIGHT) . str_pad('', '┬', 1,STR_PAD_RIGHT) . str_pad('', $spaceWork, '─') . '┬' . str_pad('', $spaceNotes, '─') . '┐';
-
-//		$output[] = str_pad('', $spaceLabel, '-', STR_PAD_RIGHT) . '-' . str_pad('', $spaceWork, '-') . '-' . str_pad('', $spaceNotes, '-') . '-';
-		/** @var $wo Work_Container */
-		foreach ($workObjects as $wo) {
-			$tmp = array();
-//			$tmp[] = '│';
-			$tmp[] = '| ';
-			$tmp[] = str_pad($wo->getLabel(), $spaceLabel, ' ', STR_PAD_RIGHT);
-//			$tmp[] = '┼';
-			$tmp[] = '|';
-			$tmp[] = str_pad(' ' . $this->getCalculator()->getHourUnit($wo->getWorkTime()).' h ', $spaceWork, ' ', STR_PAD_RIGHT);
-//			$tmp[] = '┼';
-			$tmp[] = '| ';
-			$notes = implode(', ', $wo->getNotes());
-			if (empty($notes)) {
-				$notes = '-';
-			}
-			$tmp[] = $notes;
-//			$tmp[] = '│';
-			$tmp[] = ' |';
-
-			$output[] = implode('', $tmp);
-		}
-
-		return implode(PHP_EOL, $output);
+		return $this->getCompact(time());
 	}
 
 }
--- libs/Command/Show.php
+++ libs/Command/Show.php
@@ -399,6 +399,15 @@ class Command_Show extends Command_Abstract {
 
 		if (true === $showNotes) {
 			$this->appendNotes($workObjects);
+
+			$this->appendToOutput('');
+			$this->appendToOutput('');
+			$this->appendToOutput('');
+			$this->appendToOutput(str_pad('', 30, '='));
+			$this->appendToOutput(':: Compact ::');
+			$this->appendToOutput(str_pad('', 30, '='));
+			$this->appendToOutput('');
+			$this->appendToOutput($this->getCompact($timestamp));
 		}
 
 		$output = implode(PHP_EOL, $this->finallyOutput);
@@ -440,5 +449,47 @@ class Command_Show extends Command_Abstract {
 		if (false === $hasNotes) {
 			$this->appendToOutput('- No notes available, yet.');
 		}
+	}
+
+
+	protected function getCompact($timeStamp) {
+		$workObjects = $this->getWorkedObjects($timeStamp);
+
+		if (empty($workObjects)) {
+			return 'There are no logs, yet.';
+		}
+
+		$spaceLabel = Command_Abstract::WORK_LENGTH_NAME;
+		$spaceWork = 7;
+		$spaceNotes = 30;
+
+		$output = array();
+		$output[] = '';
+		//$output[] = '┌' . str_pad('', $spaceLabel, '─', STR_PAD_RIGHT) . str_pad('', '┬', 1,STR_PAD_RIGHT) . str_pad('', $spaceWork, '─') . '┬' . str_pad('', $spaceNotes, '─') . '┐';
+
+//		$output[] = str_pad('', $spaceLabel, '-', STR_PAD_RIGHT) . '-' . str_pad('', $spaceWork, '-') . '-' . str_pad('', $spaceNotes, '-') . '-';
+		/** @var $wo Work_Container */
+		foreach ($workObjects as $wo) {
+			$tmp = array();
+//			$tmp[] = '│';
+			$tmp[] = '| ';
+			$tmp[] = str_pad($wo->getLabel(), $spaceLabel, ' ', STR_PAD_RIGHT);
+//			$tmp[] = '┼';
+			$tmp[] = '|';
+			$tmp[] = str_pad(' ' . $this->getCalculator()->getHourUnit($wo->getWorkTime()).' h ', $spaceWork, ' ', STR_PAD_RIGHT);
+//			$tmp[] = '┼';
+			$tmp[] = '| ';
+			$notes = implode(', ', $wo->getNotes());
+			if (empty($notes)) {
+				$notes = '-';
+			}
+			$tmp[] = $notes;
+//			$tmp[] = '│';
+			$tmp[] = ' |';
+
+			$output[] = implode('', $tmp);
+		}
+
+		return implode(PHP_EOL, $output);
 	}
 }
