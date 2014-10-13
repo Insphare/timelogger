332e8690d7e2ad66cc9de53905618545ef61363b
manuel <insphare@gmail.com>
2014-10-13 18:23:39 +0200

    add command compact

--- libs/Command/Show.php
+++ libs/Command/Show.php
@@ -472,7 +472,8 @@ class Command_Show extends Command_Abstract {
 		foreach ($workObjects as $wo) {
 			$label = $wo->getLabel();
 
-			switch (strtolower($label)) {
+			echo $label;exit;
+			switch (strtolower(trim($label))) {
 				case 'su':
 					$label = 'Standup';
 					break;
